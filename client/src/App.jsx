952cc2b93c9ef50d5d41dc9db00d078b9fe1b8d7
Sangam Singh Dhami <sangamdhami663@gmail.com>
2026-04-23 21:55:59 +0545

    modify the vercel file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,22 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, Suspense, lazy } from 'react'
 import { Navigate, Route, Routes, useNavigate } from 'react-router-dom'
-import Home from './pages/Home'
-import Auth from './pages/Auth'
 import { useDispatch, useSelector } from 'react-redux'
 import { getCurrentUser } from './api/user'
-import Notes from './pages/Notes'
-import History from './pages/History'
-import Credits from './pages/Credits'
+
+// Lazy loaded pages
+const Home = lazy(() => import('./pages/Home'))
+const Auth = lazy(() => import('./pages/Auth'))
+const Notes = lazy(() => import('./pages/Notes'))
+const History = lazy(() => import('./pages/History'))
+const Credits = lazy(() => import('./pages/Credits'))
 
 export const baseUrl = import.meta.env.VITE_SERVER_URL
 
 const App = () => {
 
   const dispatch = useDispatch()
-
-  
-  const { userData , isAuth} = useSelector(store=>store.user)
+  const { userData, isAuth } = useSelector(store => store.user)
   const navigate = useNavigate()
-  console.log(userData)
 
   useEffect(() => {
     getCurrentUser(dispatch)
@@ -25,18 +24,33 @@ const App = () => {
 
   useEffect(() => {
     if (isAuth) navigate("/")
-
   }, [isAuth])
-  
-  
+
   return (
-    <Routes>
-      <Route path='/' element={isAuth? <Home />: <Navigate to={'/auth'} />} />
-      <Route path='/auth' element={isAuth? <Navigate to={'/'} />: <Auth />} />
-      <Route path='/notes' element={!isAuth? <Navigate to={'/auth'} />: <Notes />} />
-      <Route path='/history' element={!isAuth? <Navigate to={'/auth'} />: <History />} />
-      <Route path='/add-credits' element={!isAuth? <Navigate to={'/auth'} />: <Credits />} />
-    </Routes>
+    <Suspense fallback={<div>Loading page...</div>}>
+      <Routes>
+        <Route
+          path='/'
+          element={isAuth ? <Home /> : <Navigate to='/auth' />}
+        />
+        <Route
+          path='/auth'
+          element={isAuth ? <Navigate to='/' /> : <Auth />}
+        />
+        <Route
+          path='/notes'
+          element={!isAuth ? <Navigate to='/auth' /> : <Notes />}
+        />
+        <Route
+          path='/history'
+          element={!isAuth ? <Navigate to='/auth' /> : <History />}
+        />
+        <Route
+          path='/add-credits'
+          element={!isAuth ? <Navigate to='/auth' /> : <Credits />}
+        />
+      </Routes>
+    </Suspense>
   )
 }
 
