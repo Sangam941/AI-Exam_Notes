ff126dc482f2de951339f8d64c622881e67ae6a6
Sangam Singh Dhami <sangamdhami663@gmail.com>
2026-04-23 20:44:26 +0545

    add the vercel origin

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(cookieParser())
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173","https://ai-exam-notes-mu.vercel.app"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"]
 }))
